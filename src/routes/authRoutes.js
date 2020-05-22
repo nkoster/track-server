2542d29085a1f45e79d7111981837b8af9850216
Niels Koster <n.koster@portavita.eu>
2020-05-22 12:03:01 +0200

    Create new user from model

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,10 +1,17 @@
 const express = require('express')
-const mongoose = require('mongoose')
 const router = express.Router()
+const mongoose = require('mongoose')
+const User = mongoose.model('User')
 
-router.post('/signup', (req, res) => {
-    console.log(req.body)
-    res.send('POST request Apekop')
+router.post('/signup', async (req, res) => {
+    const { email, password } = req.body
+    try {
+        const user = new User({ email, password })
+        await user.save()
+        res.send('POST request User')
+    } catch(err) {
+        return res.status(422).send(err.message)
+    }
 })
 
 module.exports = router
