d5aa11157a5434bcf36e1400868577f71e9eac35
Niels Koster <n.koster@portavita.eu>
2020-05-22 18:10:24 +0200

    Add signin route

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -17,4 +17,22 @@ router.post('/signup', async (req, res) => {
     }
 })
 
+router.post('/signin', async (req, res) => {
+    const { email, password } = req.body
+    if( !email || !password) {
+        return res.status(422).send({ error: 'Must provide email and password' })
+    }
+    const user = await User.findOne({ email })
+    if (!user) {
+        return res.status(422).send({ error: 'Forbidden' })
+    }
+    try {
+        await user.comparePassword(password)
+        const token = jwt.sign({ userId: user._id }, signKey)
+        res.send({ token })
+    } catch(err) {
+        return res.status(422).send({ error: 'Forbidden' })
+    }
+})
+
 module.exports = router
