4ad791fe747bbdefe7f9d5a5df43e7fc50615df5
Niels Koster <n.koster@portavita.eu>
2020-05-22 12:17:18 +0200

    Use jsonwebtoken jwt

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,14 +1,17 @@
 const express = require('express')
 const router = express.Router()
 const mongoose = require('mongoose')
+const jwt = require('jsonwebtoken')
 const User = mongoose.model('User')
+const signKey = require('../../sign-key')
 
 router.post('/signup', async (req, res) => {
     const { email, password } = req.body
     try {
         const user = new User({ email, password })
         await user.save()
-        res.send('POST request User')
+        const token = jwt.sign({ userId: user._id }, signKey)
+        res.send({ token })
     } catch(err) {
         return res.status(422).send(err.message)
     }
