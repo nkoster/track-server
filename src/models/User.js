2bb941a9a128cb5185103b71587f448d099aaf21
Niels Koster <n.koster@portavita.eu>
2020-05-22 17:10:07 +0200

    password hashing and checking stuff

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 
 const userSchema = mongoose.Schema({
     email: {
@@ -12,4 +13,37 @@ const userSchema = mongoose.Schema({
     }
 })
 
+userSchema.pre('save', function(next) {
+    const user = this
+    if (!user.isModified('password')) {
+        return next()
+    }
+    bcrypt.genSalt(10, (err, salt) => {
+        if (err) {
+            return next(err)
+        }
+        bcrypt.hash(user.password, salt, (err, hash) => {
+            if (err) {
+                return next(err)
+            }
+            user.password = hash
+            next()
+        })
+    })
+})
+
+userSchema.methods.comparePassword = function(passwordToTry) {
+    const user = this
+    return new Promise((resolve, reject) => {
+        bcrypt.compare(passwordToTry, user.password, (err, isMatch) => {
+            if (err) {
+                return reject(err)
+            }
+            if (!isMatch) {
+                return reject(false)
+            }
+            resolve(true)
+        })
+    })
+}
 mongoose.model('User', userSchema)
