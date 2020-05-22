2bc5f15040d142802355289d5f8cfed51a85b72b
Niels Koster <n.koster@portavita.eu>
2020-05-22 20:59:46 +0200

    Store track in mongo

--- src/routes/trackRoutes.js
+++ src/routes/trackRoutes.js
@@ -7,9 +7,24 @@ const Track = mongoose.model('Track')
 const router = express.Router()
 
 router.use(requireAuth)
+
 router.get('/tracks', async (req, res) => {
     const tracks = await Track.find({ userId: req.user._id })
     res.send(tracks)
+})
+
+router.post('/track', async (req, res) => {
+    const { name, locations } = req.body
+    if (!name || !locations) {
+        return res.status(422).send({ error: 'Not enough data'})
+    }
+    try {
+        const track = new Track({ name, locations, userId: req.user._id })
+        await track.save()
+        res.send(track)
+    } catch(err) {
+        res.status(422).send({ error: err.message })
+    }
 })
 
 module.exports = router
