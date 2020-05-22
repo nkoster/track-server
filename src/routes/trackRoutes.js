84b1558b60a9eb164a1ddf12ef6addbc249be433
Niels Koster <n.koster@portavita.eu>
2020-05-22 19:43:23 +0200

    Track routes and model

---
+++ src/routes/trackRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const requireAuth = require('../middleware/requireAuth')
+
+const Track = mongoose.model('Track')
+
+const router = express.Router()
+
+router.use(requireAuth)
+router.get('/tracks', async (req, res) => {
+    const tracks = await Track.find({ userId: req.user._id })
+    res.send(tracks)
+})
+
+module.exports = router
